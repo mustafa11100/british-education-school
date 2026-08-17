658ed804ec6e0293c08362108d9e41be569e86d4
mustafa11100 <sumiaadel111@gmail.com>
2026-08-17 23:59:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,23 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/api/health", (req, res) => {
+  res.json({
+    success: true,
+    message: "School Portal is running"
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("School Portal is running");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`School Portal running on port ${PORT}`);
+});
 app.use(express.json());
 
 app.get("/", (req, res) => {
